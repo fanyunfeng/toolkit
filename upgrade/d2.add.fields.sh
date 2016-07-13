bb0fd886cc282fe4aa3016722a340dc31c834429
fanyunfeng <fanyunfeng.ce@gmail.com>
2016-07-13 11:58:40 +0800

    modify item size.

--- upgrade/d2.add.fields.sh
+++ upgrade/d2.add.fields.sh
@@ -2,7 +2,8 @@
 
 
 
-sed d2_config.xml -i -e '/sign="FS"/a\\t\t\t\t<value sign="PS" dot_num="0" flag="0" type="0">0</value>'\
+sed d2_config.xml -i -e '/numeric/s/39/42/'\
+ -e '/sign="FS"/a\\t\t\t\t<value sign="PS" dot_num="0" flag="0" type="0">0</value>'\
  -e '/sign="FS"/a\\t\t\t\t<value sign="PO" dot_num="0" flag="0" type="0">0</value>'\
  -e '/sign="FS"/a\\t\t\t\t<value sign="PH" dot_num="0" flag="0" type="0">0</value>'
 
